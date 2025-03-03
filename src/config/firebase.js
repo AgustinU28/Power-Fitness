a329db6fa6a8840d8a562ed9abc16d2aecd42abf
agustinuranga@live.com <agustinuranga@live.com>
2025-03-02 21:28:28 -0300

    .env

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,17 +3,26 @@ import { initializeApp } from 'firebase/app';
 import { get } from 'firebase/database';  
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { getDatabase, ref, set } from 'firebase/database';
+import { 
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_DATABASE_URL,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID
+} from '@env';
 
-// Tu configuración de Firebase - Reemplaza con tus propias credenciales
+// Configuración de Firebase con variables de entorno
 const firebaseConfig = {
-    apiKey: "AIzaSyC5oOJKWpG-8XeNJxETzw6E_jiqeEXh3sA",
-    authDomain: "power-fittness.firebaseapp.com", // Basado en el nombre probable del proyecto
-    databaseURL: "https://power-fittness-default-rtdb.firebaseio.com/",
-    projectId: "project-385751675271", 
-    storageBucket: "power-fittness.appspot.com", // Basado en el nombre probable del proyecto
-    messagingSenderId: "385751675271", // Extraído del appId
-    appId: "1:385751675271:web:7a01b08695b9b724af6322"
-  };
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN, 
+    databaseURL: FIREBASE_DATABASE_URL,
+    projectId: FIREBASE_PROJECT_ID, 
+    storageBucket: FIREBASE_STORAGE_BUCKET, 
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID, 
+    appId: FIREBASE_APP_ID
+};
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
